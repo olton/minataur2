d10bac7247ac6bc737bcdc9de8aa42753d79dc53
olton <serhii@pimenov.com.ua>
2023-09-12 11:20:14 +0300

    added transaction

---
+++ server/Views/js/transactions/charts.js
@@ -0,0 +1,169 @@
+;
+const chartOptions = {
+    height: 100,
+    title: false,
+    padding: {
+        bottom: 20,
+        right: 20,
+        top: 20,
+    },
+    axis: {
+        y: {
+            label: {
+                fixed: 0,
+                count: 5,
+                step: "auto",
+                font: {
+                    size: 10
+                }
+            }
+        },
+        x: {
+            label: false
+        },
+    },
+    arrows: false,
+    legend: false,
+    border: false,
+}
+
+const updateTransCharts = (data) => {
+    updateChartFees(data)
+    updateChartAmount(data)
+    updateChartStatus(data)
+}
+
+const updateChartTransInBlock = data => {
+    let _data = []
+    let index = 0
+    for(let r of data.reverse()) {
+        _data.push([+r.height, +r.user_trans_count])
+        index++
+    }
+    const areas = [
+        {
+            name: "TPB"
+        }
+    ]
+    chart.lineChart("#chart-trans-in-block", [_data], {
+        lines: areas,
+        ...chartOptions,
+        colors: [chart.defaultColors.coral],
+        boundaries: {
+            minY: 0
+        },
+        onTooltipShow: (data) => {
+            const [block, transactionsCount] = data
+            return `<span>Block: ${block}, Trans: ${transactionsCount}</span>`
+        }
+    })
+}
+
+const updateChartFees = data => {
+    let _data = []
+    let index = 0
+    for(let r of data.reverse()) {
+        _data.push([index, +r.fee])
+        index++
+    }
+    const areas = [
+        {
+            name: "TPB"
+        }
+    ]
+    chart.lineChart("#chart-trans-fee", [_data], {
+        lines: areas,
+        ...chartOptions,
+        colors: [chart.defaultColors.mediumSlateBlue],
+        boundaries: {
+            minY: 0
+        },
+        padding: {
+            bottom: 20,
+            right: 20,
+            top: 20,
+            left: 60,
+        },
+        onTooltipShow: (data) => {
+            const [block, transactionsCount] = data
+            return `<span>Block: ${block}, Fee: ${transactionsCount}</span>`
+        },
+        onDrawLabelY: (val) => {
+            return (val / 10**9).toFixed(4)
+        }
+    })
+}
+
+const updateChartAmount = data => {
+    let _data = []
+    let index = 0
+    for(let r of data.reverse()) {
+        _data.push([index, +r.amount])
+        index++
+    }
+    const areas = [
+        {
+            name: "TPB"
+        }
+    ]
+    chart.lineChart("#chart-trans-amount", [_data], {
+        lines: areas,
+        ...chartOptions,
+        colors: [chart.defaultColors.deepSkyBlue],
+        boundaries: {
+            minY: 0
+        },
+        padding: {
+            bottom: 20,
+            right: 20,
+            top: 20,
+            left: 60,
+        },
+        onTooltipShow: (data) => {
+            const [block, transactionsCount] = data
+            return `<span>Block: ${block}, Amount: ${transactionsCount}</span>`
+        },
+        onDrawLabelY: (val) => {
+            return (val / 10**9).toFixed(4)
+        }
+    })
+}
+
+const updateChartStatus = data => {
+    let applied = 0, failed = 0
+    let index = 0
+    for(let r of data.reverse()) {
+        if (r.status === 'applied') applied++
+        if (r.status === 'failed') failed++
+        index++
+    }
+    console.log(applied, failed)
+    chart.barChart("#chart-trans-status", [applied, failed], {
+        height: 100,
+        bars: ["Applied", "Failed"],
+        colors: ["green", "red"],
+        title: false,
+        barDistance: 10,
+        groupDistance: 10,
+        arrows: false,
+        border: false,
+        boundaries: {
+            max: applied + failed
+        },
+        padding: {
+            top: 10
+        },
+        axis: {
+            y: {
+                label: {
+                    fixed: 0,
+                    count: 5,
+                    step: "auto",
+                    font: {
+                        size: 10
+                    }
+                }
+            }
+        }
+    })
+}
--- server/Views/js/transactions/wsc.js
+++ server/Views/js/transactions/wsc.js
@@ -5,17 +5,26 @@ globalThis.wsController = (ws, res) => {
         case "welcome": {
             $(".live").show((el)=>$(el).css("display", "flex"))
             log(`Welcome to Minataur Server!`)
-            console.log(createTransRequest())
             request("user_transactions", createTransRequest())
+            request("block_stats")
+            break
+        }
+        case "new_block": {
+            request("block_stats")
             break
         }
         case "user_transactions": {
             updateTransTable(data)
+            updateTransCharts(data.rows)
             enableElements()
             updateInterval = setTimeout(() => {
                 clearUpdateInterval()
                 request("user_transactions", createTransRequest())
             }, 60000)
+            break
+        }
+        case "block_stats": {
+            updateChartTransInBlock(data)
             break
         }
     }
