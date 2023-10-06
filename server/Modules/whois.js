89dedac9ef8ae7cfe9bb7f16a8ec99e2c6cb1e88
Serhii Pimenov <serhii@pimenov.com.ua>
2023-10-06 21:05:54 +0300

    fixed whois updater

--- server/Modules/whois.js
+++ server/Modules/whois.js
@@ -17,21 +17,24 @@ export const updateWhois = async ({f1: sender_id, f2: receiver_key, f3, f4: amou
         //if (+amount < 1) return
         if (receiver_key !== MINATAUR_ADDRESS) return
         const memo = decodeMemo(f3)
-        const fields = ['name', 'site', 'telegram', 'discord', 'email', 'twitter', 'github', 'logo', 'description']
+        const fields = ['name', 'site', 'telegram', 'discord', 'email', 'twitter', 'github', 'logo', 'desc']
         const data = memo.split(";")
         const query_fields = []
         for (let part of data) {
             const [field, value] = part.split(":")
             if (!fields.includes(field)) continue
-            query_fields.push(`${field} = '${value}'`)
+            query_fields.push(`"${field}" = '${value}'`)
         }
         if (query_fields.length === 0) return
+        const sender_exists = (await query(`select count(*) as length from whois where public_key_id = $1`, [sender_id])).rows[0].length > 0
+        if (!sender_exists) {
+            await query(`insert into whois (public_key_id) values ($1)`, [sender_id])
+        }
         const sql = `
             update whois
             set ${query_fields.join(", ")}
             where public_key_id = $1
         `
-        console.log(sql)
         query(sql, [sender_id])
     } catch (e) {
         return null
