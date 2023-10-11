13be40131d8be55a1c5161baae06738559c202c3
Serhii Pimenov <serhii@pimenov.com.ua>
2023-10-11 20:00:32 +0300

    improved balance updater

--- server/Modules/events.js
+++ server/Modules/events.js
@@ -7,7 +7,7 @@ import {
     cache_epoch, cache_last_canonical_block
 } from "./cache.js";
 import {updateWhois} from "./whois.js";
-import {db_get_block_trans} from "./db.js";
+import {db_get_block_internal_commands, db_get_block_trans} from "./db.js";
 import {ql_get_account_balance} from "./graphql.js";
 import {query} from "./postgres.js";
 
@@ -28,7 +28,9 @@ export const on_new_user_tx_memo = (data) => {
 
 export const on_canonical_block = async (data) => {
     const block_id = data.id
-    const tx = await db_get_block_trans(block_id)
+
+    // Process User Transactions
+    let tx = await db_get_block_trans(block_id)
     for(let row of tx) {
         const {sender_id, receiver_id, sender_key, receiver_key} = row
 
@@ -40,6 +42,20 @@ export const on_canonical_block = async (data) => {
                 on conflict do nothing
             `, [sender_id, block_id, sender_balance.total, sender_balance.liquid, sender_balance.locked, sender_balance.unknown])
         }
+        const receiver_balance = await ql_get_account_balance(receiver_key)
+        if (receiver_balance) {
+            query(`
+                insert into balances (public_key_id, block_id, total, liquid, locked, unknown)
+                values ($1, $2, $3, $4, $5, $6)
+                on conflict do nothing
+            `, [receiver_id, block_id, receiver_balance.total, receiver_balance.liquid, receiver_balance.locked, receiver_balance.unknown])
+        }
+    }
+
+    // Process coinbase
+    tx = await db_get_block_internal_commands(block_id)
+    for(let row of tx) {
+        const {receiver_id, receiver_key} = row
         const receiver_balance = await ql_get_account_balance(receiver_key)
         if (receiver_balance) {
             query(`
