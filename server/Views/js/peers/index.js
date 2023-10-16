ecbb118086d9e1039d2f05ebbf93df1a97f886aa
Serhii Pimenov <serhii@pimenov.com.ua>
2023-10-16 14:24:15 +0300

    improve peers

--- server/Views/js/peers/index.js
+++ server/Views/js/peers/index.js
@@ -1,6 +1,5 @@
 ;
 const updatePeers = (data) => {
-    console.log(data)
     const location = {}
     const available = data.peers.filter(r => r.available).length
 
@@ -14,6 +13,17 @@ const updatePeers = (data) => {
 
     $("#found-peers").html(`${num2fmt(data.peers.length)}`)
     $("#active-peers").html(`${num2fmt(available)}`)
+
+    let peers = data.peers
+    peers.map( r => {
+        r.loc = location[r.host]
+    })
+
+    peers.sort((a, b)=>{
+        if (a.loc.isp > b.loc.isp) return 1
+        if (a.loc.isp < b.loc.isp) return -1
+        return 0
+    })
 
     const target = $("#peers-table tbody").clear()
     const rows = drawPeersTable(data.peers, location)
