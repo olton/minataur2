a1f30d6eb574e35be8bcafc0b91a2f9a95dceb55
olton <serhii@pimenov.com.ua>
2023-11-08 12:23:30 +0200

    upd mina client status command

--- server/Modules/shell.js
+++ server/Modules/shell.js
@@ -3,6 +3,7 @@ import {exec} from "child_process";
 export const exec_mina_client_status = cb => {
     exec("mina client status --json", (error, stdout, stderror) => {
         if (error) {
+            cb.apply(null, [null])
             return null
         }
         cb.apply(null, [stdout])
